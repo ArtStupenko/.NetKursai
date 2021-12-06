3718d0a5aa40b98c3e6d866578284d103f23ab01
ArtStupenko <artstupenko@gmail.com>
2021-12-06 20:09:24 +0200

    struct

---
+++ 7 paskaita/7_paskaita/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _7_paskaita
+{
+    internal class Program
+    {
+        public struct Vidurkis
+        {
+            public double Trimestras1;
+            public double Trimestras2;
+            public double Trimestras3;
+
+
+            public Vidurkis(double trimestras1, double trimestras2, double trimestras3)
+            {
+                Trimestras1 = trimestras1;
+                Trimestras2 = trimestras2;
+                Trimestras3 = trimestras3;
+            }
+            public void KoksVidurkis()
+            {
+                double Metinis;
+                Metinis = Math.Round(((Trimestras1 + Trimestras2 + Trimestras3) / 3), 2);
+                Console.WriteLine($"Vidurkis : {Metinis}");
+            }
+
+            static void Main(string[] args)
+            {
+                Vidurkis vidurkis;
+                vidurkis.Trimestras1 = 9;
+                vidurkis.Trimestras2 = 6;
+                vidurkis.Trimestras3 = 4;
+
+                Vidurkis vidurkis1 = new Vidurkis(8, 7, 6);
+
+                vidurkis.KoksVidurkis();
+                vidurkis1.KoksVidurkis();
+
+            }
+
+
+        }
+    }
+}
